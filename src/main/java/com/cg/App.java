eb1e2c5fea084e0463e08eeae0c8a09e1ef987f8
riteshcg22 <ritesh-raj.keshari@capgemini.com>
2022-05-11 18:04:38 +0530

    2nd commit

--- src/main/java/com/cg/App.java
+++ src/main/java/com/cg/App.java
@@ -9,6 +9,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        System.out.println("Wonderfu;ll");
     }
     public int add(int a, int b) {
     	return a+b;
